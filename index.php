867aa1212bc6b7fd97b95b25dd6cb69aeb4e8c54
Tim Bogers <tim.bogers1@edu-kw1c.nl>
2020-02-10 11:02:21 +0100

    Template gemaakt, werkomgeving

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * User: ${USER}
+ * Date: ${DATE}
+ * Time: ${TIME}
+ * File: ${PROJECT_NAME}
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+
+    </title>
+
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+    <link href="CSS/style.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+<h2><?php echo "Uitwerking van PHP-opdrachten"; ?></h2>
+
+<div class="box">
+    <ul class="directory-list">
+        <li>Document
+            <ul>
+                <li>Hoofdstuk 2
+                    <ul>
+                        <li><a href="hoofdstuk2/Opdracht_2.1.php">Opdracht 2.1</a></li>
+                        <li><a href="hoofdstuk2/Opdracht_2.2.php">Opdracht 2.2</a></li>
+                    </ul>
+                </li>
+                <li>Hoofdstuk 3
+                    <ul>
+                        <li><a href="hoofdstuk3/Opdracht_3.1.php">Opdracht 3.1</a></li>
+                        <li><a href="hoofdstuk3/Opdracht_3.2.php">Opdracht 3.2</a></li>
+                        <li><a href="hoofdstuk3/Opdracht_3.3.php">Opdracht 3.3</a></li>
+                    </ul>
+                </li>
+                <li>Hoofdstuk 4
+                    <ul>
+                        <li><a href="hoofdstuk4/Opdracht_4.1.php">Opdracht 4.1</a></li>
+                        <li><a href="hoofdstuk4/Opdracht_4.2.php">Opdracht 4.2</a></li>
+                        <li><a href="hoofdstuk4/Opdracht_4.3.php">Opdracht 4.3</a></li>
+                        <li><a href="hoofdstuk4/Opdracht_4.4.php">Opdracht 4.4</a></li>
+                    </ul>
+                </li>
+                <li>Hoofdstuk 5
+                    <ul>
+                        <li><a href="hoofdstuk5/Opdracht_5.1.php">Opdracht 5.1</a></li>
+                        <li><a href="hoofdstuk5/Opdracht_5.2.php">Opdracht 5.2</a></li>
+                        <li><a href="hoofdstuk5/Opdracht_5.3.php">Opdracht 5.3</a></li>
+                        <li><a href="hoofdstuk5/Opdracht_5.4.php">Opdracht 5.4</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</div>
+</body>
+</html>
