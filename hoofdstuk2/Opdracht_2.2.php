277db1f3e490a05ec319dee8c589f4e3e532169d
Tim Bogers <tim.bogers1@edu-kw1c.nl>
2020-02-10 11:41:03 +0100

    Opdracht 2.1 af

---
+++ hoofdstuk2/Opdracht_2.2.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * User: timbo
+ * Date: 10-2-2020
+ * Time: 11:13
+ * File: Opdracht_2.2.php
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+
+    </title>
+
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+    <link href="../CSS/style.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+<h2>Uitwerking van <a href="../index.php"> PHP-opdrachten</a></h2>
+
+<div class="box">
+    <?php
+        $text1 = "Hallo";
+        $text2 = "een makkelijke taal";
+        $text3 = "toch zo'n makkelijke taal";
+        $text4 = "wat is";
+        $text5 = "PHP";
+        $text6 = "Nooit gedacht dat";
+        $text7 = "De installatie is best ingewikkeld";
+        $text8 = "Fijn";
+        $text9 = "?";
+        $text10 = ".";
+        $text11 = ",";
+        $text12 = "<br>";
+        $text13 = "is";
+        $text14 = "Vind je niet";
+        echo ("<p> $text1$text11 $text4 $text5 toch $text2$text10 $text12 $text7$text10 $text8 toch$text9 $text12 $text6 $text5 $text3 $text13$text10</p>")
+    ?>
+</div>
+</body>
+</html>
