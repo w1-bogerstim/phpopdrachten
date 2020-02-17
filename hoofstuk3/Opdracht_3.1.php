c19320dab57e06f010021ca5ee3aec82e672770e
Tim Bogers <tim.bogers1@edu-kw1c.nl>
2020-02-17 11:58:04 +0100

    Opdracht 2.2 af

--- hoofstuk3/Opdracht_3.1.php
+++ hoofstuk3/Opdracht_3.1.php
@@ -24,7 +24,21 @@
 <h2>Uitwerking van <a href="../index.php"> PHP-opdrachten</a></h2>
 
 <div class="box">
+<?php
+    $text1 = "Elfstedentocht";
+    $text2 = "Alvestêdetocht";
+    $text3 = "200";
+    $text4 = "schaatstocht";
+    $text5 = "natuurijs";
+    $text6 = "Koninklijke Vereniging De Frische Elf Steden";
+    $text7 = "Leeuwarden";
+    $text8 = "Friesland";
+    $text9 = "15";
+    $text10 = "1909";
+    $text11 = "1";
 
+    $verhaal1 = "De $text1 (Fries: $text2 ) is toch een $text3 kilometer lange $text4 over $text5 die wordt georganiseerd door de $text6. $text7, de hoofdstad van $text8, is start- en aankomstplaats. De $text1 is inmiddels $text9 maal verreden en wrd voor het eerst in $text10 gereden en wordt maximaal $text11 keer per winter gehouden."
+?>
 </div>
 </body>
 </html>
