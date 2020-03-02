85cafdd6cf3e215ff41a80ef18f541341b5d0688
Tim Bogers <tim.bogers1@edu-kw1c.nl>
2020-03-02 12:36:23 +0100

    Opdracht 3.3 af

---
+++ hoofdstuk3/Opdracht_3.3.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: timbo
+ * Date: 2-3-2020
+ * Time: 12:19
+ * File: Opdracht_3.3.php
+ */
+?>
+<?php
+include '../includes/header.php';
+?>
+
+<?php
+include '../includes/footer.php';
+?>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+
+    </title>
+
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+    <link href="../CSS/style.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+<h2>Uitwerking van <a href="../index.php"> PHP-opdrachten</a></h2>
+
+<div class="box">
+
